ca08a22f3e727bbba11423256bd2d056979b14f8
Konsulten <nordmarkclaes@gmail.com>
2026-03-08 08:18:23 +0100

    actually removed the third party lib...

--- components/balboa_spa/__init__.py
+++ components/balboa_spa/__init__.py
@@ -49,7 +49,6 @@ CONFIG_SCHEMA = cv.Schema({
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    cg.add_library(name="rlogiacco/CircularBuffer", version=None)
     await uart.register_uart_device(var, config)
 
 
